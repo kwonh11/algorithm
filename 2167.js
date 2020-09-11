964678ccaddf324be5ed772355402a68260885b2
kwonh11 <kwonh11@gmail.com>
2020-09-11 18:04:45 +0900

    3주차 알고리즘 완료

--- 2167.js
+++ 2167.js
@@ -8,17 +8,27 @@ let arr = [];
 let N, M;   // N행 M열
 let K;      // parts 배열의 크기
 let parts = [];
-const toNumber = (v) => Number(v);
 rl.on('line', function (line) {
-    input.push(line.split(' ').map(toNumber));
+    input.push(line.split(' ').map(v=>Number(v)));
     [N, M] = input[0];
     K = Number(input[N+1]);
     if(input.length > N + K + 1) {
         const results = [];
-        arr = input.slice(1, N+1);
+        array = input.slice(1, N+1);
         parts = input.slice(N+2);
-
-        
+        parts.forEach(part => {
+            const [i,j,x,y] = part.map(v => v-1);
+            let result = 0;
+            array.forEach((arr,a)=>{
+                arr.forEach((number,b)=>{
+                    if(a>=i && a<=x && b>=j && b<=y) {
+                        result += number;
+                    }
+                })
+            })
+            results.push(result);
+        })
+        results.forEach(result => console.log(result));
         rl.close();
     }
 })
